ec71101a26bda729635751899feeeb0332fd16b8
Sharafiddin.Mukhtorov <129146459+SharafiddinMukhtorov@users.noreply.github.com>
2024-07-25 17:01:14 +0500

    Created entity

---
+++ CheckDrive.Api/CheckDrive.Domain/Mappings/OilMarkMappings.cs
@@ -0,0 +1,18 @@
+﻿using CheckDrive.ApiContracts.OilMark;
+using CheckDrive.Domain.Entities;
+using AutoMapper;
+using CheckDrive.ApiContracts.Role;
+
+namespace CheckDrive.Domain.Mappings
+{
+    public class OilMarkMappings : Profile
+    {
+        public OilMarkMappings()
+        {
+            CreateMap<OilMarkDto, Role>();
+            CreateMap<Role, OilMarkDto>();
+            CreateMap<OilMarkForCreateDto, Role>();
+            CreateMap<OilMarkForUpdateDto, Role>();
+        }
+    }
+}
---
+++ CheckDrive.Api/CheckDrive.Infrastructure/Persistence/Configurations/OilMarkConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using CheckDrive.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CheckDrive.Infrastructure.Persistence.Configurations
+{
+    public class OilMarkConfiguration : IEntityTypeConfiguration<OilMarks>
+    {
+        public void Configure(EntityTypeBuilder<OilMarks> builder)
+        {
+            builder.ToTable(nameof(Role));
+            builder.HasKey(x => x.Id);
+
+
+            builder.Property(x => x.OilMark)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            builder.HasMany(r => r.OperatorReviews)
+                .WithOne(a => a.OilMarks);
+        }
+    }
+}
