4c08d53805221082fb19c45b011fd938c34c6774
khojianvar <khojianvarabdumukhtorov@gmail.com>
2024-07-31 15:54:39 +0500

    Add Property for Export Excal file

---
+++ CheckDrive.Api/CheckDrive.ApiContracts/PropertyForExportFile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CheckDrive.ApiContracts
+{
+    public class PropertyForExportFile
+    {
+        public int Month { get; set; }
+        public int Year { get; set; }
+    }
+}
