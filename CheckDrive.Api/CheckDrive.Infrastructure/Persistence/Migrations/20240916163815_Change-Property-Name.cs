fae5fc7ff5587f90e2bf54c5fd653f20c4114d63
khojianvar <khojianvarabdumukhtorov@gmail.com>
2024-09-17 10:18:42 +0500

    Update-Entity-In-MechanicAcceptance

---
+++ CheckDrive.Api/CheckDrive.Infrastructure/Persistence/Migrations/20240916163815_Change-Property-Name.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CheckDrive.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangePropertyName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RemainingFuel",
+                table: "MechanicAcceptance",
+                newName: "RemainingFuelInCar");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RemainingFuelInCar",
+                table: "MechanicAcceptance",
+                newName: "RemainingFuel");
+        }
+    }
+}
