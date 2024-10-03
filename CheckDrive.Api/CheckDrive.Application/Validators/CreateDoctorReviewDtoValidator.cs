c9b92fefdfb0f7203eb5b01add926128905ab5d7
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-10-03 14:55:18 +0500

    Add CreateDoctorReviewDtoValidator

---
+++ CheckDrive.Api/CheckDrive.Application/Validators/CreateDoctorReviewDtoValidator.cs
@@ -0,0 +1,12 @@
+﻿using CheckDrive.Application.DTOs.DoctorReview;
+using FluentValidation;
+
+namespace CheckDrive.Application.Validators;
+public class CreateDoctorReviewDtoValidator : AbstractValidator<CreateDoctorReviewDto> 
+{
+    public CreateDoctorReviewDtoValidator()
+    {
+        RuleFor(x => x.DriverId)
+            .GreaterThan(0).WithMessage("Haydovchini tanlashingiz shart.");
+    }
+}
