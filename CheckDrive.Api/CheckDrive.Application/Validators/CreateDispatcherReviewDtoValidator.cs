4a16291b651119d3ee681ea1472c02ee0e151d7d
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-10-03 15:04:39 +0500

    Add CreateDispatcherReviewDtoValidator

---
+++ CheckDrive.Api/CheckDrive.Application/Validators/CreateDispatcherReviewDtoValidator.cs
@@ -0,0 +1,16 @@
+﻿using CheckDrive.Application.DTOs.DispatcherReview;
+using FluentValidation;
+
+public class CreateDispatcherReviewDtoValidator : AbstractValidator<CreateDispatcherReviewDto>
+{
+    public CreateDispatcherReviewDtoValidator()
+    {
+        RuleFor(x => x.DistanceTravelledAdjustment)
+            .GreaterThanOrEqualTo(0).When(x => x.DistanceTravelledAdjustment.HasValue)
+            .WithMessage("Masofa o'zgartirilishi manfiy qiymat bo'lishi mumkin emas.");
+
+        RuleFor(x => x.FuelConsumptionAdjustment)
+            .GreaterThanOrEqualTo(0).When(x => x.FuelConsumptionAdjustment.HasValue)
+            .WithMessage("Yoqilgi sarfi o'zgartirilishi manfiy qiymat bo'lishi mumkin emas.");
+    }
+}
