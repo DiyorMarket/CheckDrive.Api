66f09a76016a32c207f5f617b0e66ec92f40f233
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-10-03 15:01:52 +0500

    Add CreateMechanicAcceptanceReviewDtoValidator

---
+++ CheckDrive.Api/CheckDrive.Application/Validators/CreateMechanicAcceptanceReviewDtoValidator.cs
@@ -0,0 +1,20 @@
+﻿using CheckDrive.Application.DTOs.MechanicAcceptance;
+using FluentValidation;
+
+public class CreateMechanicAcceptanceReviewDtoValidator : AbstractValidator<CreateMechanicAcceptanceReviewDto>
+{
+    public CreateMechanicAcceptanceReviewDtoValidator()
+    {
+        RuleFor(x => x.DriverId)
+            .GreaterThan(0).WithMessage("Haydovchini tanlashingiz shart.");
+
+        RuleFor(x => x.FinalMileage)
+            .GreaterThan(0).WithMessage("Spidometrdagi masofani kiritishingiz shart.");
+
+        RuleFor(x => x.RemainingFuelAmount)
+            .GreaterThanOrEqualTo(0).WithMessage("Yoqilgi qoldigi miqdorini kiritishingiz shart va manfiy qiymat qabul qilinmaydi.");
+
+        RuleFor(x => x.DebtAmountFromDriver)
+            .GreaterThanOrEqualTo(0).WithMessage("Qarzdorlik miqdori manfiy bo'lmasligi kerak.");
+    }
+}
