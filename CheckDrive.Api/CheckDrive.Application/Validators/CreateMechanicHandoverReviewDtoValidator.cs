b58eb2bccc74c87d9ca5cecb15e9b8cefdf0b7c9
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-10-03 10:15:27 +0500

    Add CreateMechanicHandoverReviewDtoValidator

---
+++ CheckDrive.Api/CheckDrive.Application/Validators/CreateMechanicHandoverReviewDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using CheckDrive.Application.DTOs.MechanicHandover;
+using FluentValidation;
+
+public class CreateMechanicHandoverReviewDtoValidator : AbstractValidator<CreateMechanicHandoverReviewDto>
+{
+    public CreateMechanicHandoverReviewDtoValidator()
+    {
+        RuleFor(x => x.ReviewerId)
+            .GreaterThan(0).WithMessage("Haydovchini tanlashingiz shart.");
+
+        RuleFor(x => x.CarId)
+            .GreaterThan(0).WithMessage("Moshinani tanlashingiz shart.");
+
+        RuleFor(x => x.InitialMileage)
+            .GreaterThan(0).WithMessage("Spidometrdagi masofani kiritishingiz shart.");
+    }
+}
