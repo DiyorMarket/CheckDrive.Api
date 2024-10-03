98777067b7dc0615345d31b60ba185c1f0f86049
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-10-03 14:59:26 +0500

    Add CreateOperatorReviewDtoValidator

---
+++ CheckDrive.Api/CheckDrive.Application/Validators/CreateOperatorReviewDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using CheckDrive.Application.DTOs.OperatorReview;
+using FluentValidation;
+
+public class CreateOperatorReviewDtoValidator : AbstractValidator<CreateOperatorReviewDto>
+{
+    public CreateOperatorReviewDtoValidator()
+    {
+        RuleFor(x => x.DriverId)
+            .GreaterThan(0).WithMessage("Haydovchini tanlashingiz shart.");
+
+        RuleFor(x => x.OilRefillAmount)
+            .GreaterThanOrEqualTo(0).WithMessage("Yoqilgi miqdorini kiritishingiz shart va manfiy qiymat bo'lishi mumkin emas.");
+
+        RuleFor(x => x.InitialOilAmount)
+            .GreaterThanOrEqualTo(0).WithMessage("Yoqilgi qoldigi miqdorini kiritishingiz shart va manfiy qiymat bo'lishi mumkin emas.");
+    }
+}
