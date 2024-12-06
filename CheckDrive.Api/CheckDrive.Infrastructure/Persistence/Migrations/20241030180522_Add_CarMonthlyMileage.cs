fcaf1bd9a678714608ef2b1f589628b5ad525410
FirdavsAX <137472686+FirdavsAX@users.noreply.github.com>
2024-12-06 21:52:39 +0500

    change returned in migrations

--- CheckDrive.Api/CheckDrive.Infrastructure/Persistence/Migrations/20241030180522_Add_CarMonthlyMileage.cs
+++ CheckDrive.Api/CheckDrive.Infrastructure/Persistence/Migrations/20241030180522_Add_CarMonthlyMileage.cs
@@ -4,27 +4,28 @@
 
 #pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
 
-namespace CheckDrive.Infrastructure.Persistence.Migrations;
-
-/// <inheritdoc />
-public partial class Add_CarMonthlyMileage : Migration
+namespace CheckDrive.Infrastructure.Persistence.Migrations
 {
     /// <inheritdoc />
-    protected override void Up(MigrationBuilder migrationBuilder)
+    public partial class Add_CarMonthlyMileage : Migration
     {
-        migrationBuilder.AddColumn<int>(
-            name: "CurrentMonthMileage",
-            table: "Car",
-            type: "int",
-            nullable: false,
-            defaultValue: 0);
-    }
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CurrentMonthMileage",
+                table: "Car",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
 
-    /// <inheritdoc />
-    protected override void Down(MigrationBuilder migrationBuilder)
-    {
-        migrationBuilder.DropColumn(
-            name: "CurrentMonthMileage",
-            table: "Car");
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CurrentMonthMileage",
+                table: "Car");
+        }
     }
 }
