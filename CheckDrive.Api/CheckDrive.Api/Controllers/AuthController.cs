f91eb656760f383eb56c37fe0ebba04b97776ec1
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2024-12-31 14:34:33 +0500

    add refresh token endpoint

--- CheckDrive.Api/CheckDrive.Api/Controllers/AuthController.cs
+++ CheckDrive.Api/CheckDrive.Api/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using CheckDrive.Application.DTOs.Identity;
+﻿using CheckDrive.Application.DTOs.Auth;
+using CheckDrive.Application.DTOs.Identity;
 using CheckDrive.Application.Interfaces.Auth;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,6 +20,14 @@ public class AuthController : ControllerBase
     public async Task<IActionResult> LoginAsync([FromBody] LoginDto request)
     {
         var token = await _authService.LoginAsync(request);
+
+        return Ok(token);
+    }
+
+    [HttpPost("refresh-token")]
+    public async Task<ActionResult<TokenDto>> RefreshTokenAsync([FromBody] RefreshTokenRequest request)
+    {
+        var token = await _authService.RefreshTokenAsync(request);
 
         return Ok(token);
     }
