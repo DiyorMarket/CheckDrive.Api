2ea236a075997e74e25df524e28981b0fc472f04
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2025-02-02 00:20:43 +0500

    migrations

---
+++ CheckDrive.Api/CheckDrive.Infrastructure/Persistence/Migrations/20250131121502_Change_Distnace_And_Amounts_To_Decimal.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CheckDrive.Infrastructure.Persistence.Migrations;
+
+/// <inheritdoc />
+public partial class Change_Distnace_And_Amounts_To_Decimal : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AlterColumn<decimal>(
+            name: "InitialMileage",
+            table: "MechanicHandover",
+            type: "decimal(18,2)",
+            nullable: false,
+            oldClrType: typeof(int),
+            oldType: "int");
+
+        migrationBuilder.AlterColumn<decimal>(
+            name: "FinalMileage",
+            table: "MechanicAcceptance",
+            type: "decimal(18,2)",
+            nullable: false,
+            oldClrType: typeof(int),
+            oldType: "int");
+
+        migrationBuilder.AlterColumn<decimal>(
+            name: "InitialMileage",
+            table: "ManagerReview",
+            type: "decimal(18,2)",
+            nullable: false,
+            oldClrType: typeof(int),
+            oldType: "int");
+
+        migrationBuilder.AlterColumn<decimal>(
+            name: "FinalMileage",
+            table: "ManagerReview",
+            type: "decimal(18,2)",
+            nullable: false,
+            oldClrType: typeof(int),
+            oldType: "int");
+
+        migrationBuilder.AlterColumn<decimal>(
+            name: "FinalMileage",
+            table: "DispatcherReview",
+            type: "decimal(18,2)",
+            nullable: false,
+            oldClrType: typeof(int),
+            oldType: "int");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AlterColumn<int>(
+            name: "InitialMileage",
+            table: "MechanicHandover",
+            type: "int",
+            nullable: false,
+            oldClrType: typeof(decimal),
+            oldType: "decimal(18,2)");
+
+        migrationBuilder.AlterColumn<int>(
+            name: "FinalMileage",
+            table: "MechanicAcceptance",
+            type: "int",
+            nullable: false,
+            oldClrType: typeof(decimal),
+            oldType: "decimal(18,2)");
+
+        migrationBuilder.AlterColumn<int>(
+            name: "InitialMileage",
+            table: "ManagerReview",
+            type: "int",
+            nullable: false,
+            oldClrType: typeof(decimal),
+            oldType: "decimal(18,2)");
+
+        migrationBuilder.AlterColumn<int>(
+            name: "FinalMileage",
+            table: "ManagerReview",
+            type: "int",
+            nullable: false,
+            oldClrType: typeof(decimal),
+            oldType: "decimal(18,2)");
+
+        migrationBuilder.AlterColumn<int>(
+            name: "FinalMileage",
+            table: "DispatcherReview",
+            type: "int",
+            nullable: false,
+            oldClrType: typeof(decimal),
+            oldType: "decimal(18,2)");
+    }
+}
