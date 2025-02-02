abcb2357f6fd5f79959249031cb79f6934d0fa98
Miraziz <khidoyatov1997@gmail.com>
2025-02-02 16:43:52 +0500

    add models for monthly report

---
+++ CheckDrive.Api/CheckDrive.Application/DTOs/Reports/CarUsageReport.cs
@@ -0,0 +1,20 @@
+﻿namespace CheckDrive.Application.DTOs.Reports;
+
+public sealed class CarUsageReport
+{
+    public string CarName { get; set; }
+    public string OilMark { get; set; }
+    public decimal AverageFuelConsumptionLimit { get; set; }
+    public decimal TotalFuelAdded { get; set; }
+    public decimal ActualFuelConsumed { get; set; }
+    public int TotalDistanceTraveled { get; set; }
+    public decimal ExpectedFuelConsumption { get; set; }
+    public decimal StartingFuelLeftover { get; set; }
+    public decimal EndingFuelLeftover { get; set; }
+
+    public CarUsageReport()
+    {
+        CarName = string.Empty;
+        OilMark = string.Empty;
+    }
+}
