818bce9523d779b704865c7140112fa1b76ff8fc
Miraziz <khidoyatov1997@gmail.com>
2025-02-02 16:45:24 +0500

    Update DevelopmentDatabaseSeeder.cs

--- CheckDrive.Api/CheckDrive.TestDataCreator/Seeders/DevelopmentDatabaseSeeder.cs
+++ CheckDrive.Api/CheckDrive.TestDataCreator/Seeders/DevelopmentDatabaseSeeder.cs
@@ -1,4 +1,5 @@
-﻿using CheckDrive.Domain.Entities;
+﻿using Bogus;
+using CheckDrive.Domain.Entities;
 using CheckDrive.Domain.Interfaces;
 using CheckDrive.TestDataCreator.Configurations;
 using CheckDrive.TestDataCreator.Interfaces;
@@ -8,25 +9,35 @@ namespace CheckDrive.TestDataCreator.Seeders;
 
 internal sealed class DevelopmentDatabaseSeeder : IDatabaseSeeder
 {
+    private static readonly Faker _faker;
+
+    static DevelopmentDatabaseSeeder()
+    {
+        _faker = new Faker();
+    }
+
     private static readonly List<string> _oilMarks = [
-        "80", "90", "95", "98", "100", "110"
+        "80", "90", "95", "98", "100", "110", "Diesel"
     ];
 
     public async Task SeedDatabaseAsync(ICheckDriveDbContext context, UserManager<IdentityUser> userManager, DataSeedOptions options)
     {
-        CreateOilMarks(context);
-        CreateCars(context, options);
-        CreateDrivers(context, userManager, options);
-        CreateDoctors(context, userManager, options);
-        CreateMechanics(context, userManager, options);
-        CreateOperators(context, userManager, options);
-        CreateDispatchers(context, userManager, options);
-        CreateManagers(context, userManager, options);
+        await CreateOilMarksAsync(context);
+        await CreateCarsAsync(context, options);
+        await CreateDriversAsync(context, userManager, options);
+        await CreateDoctorsAsync(context, userManager, options);
+        await CreateDoctorsAsync(context, userManager, options);
+        await CreateDoctorsAsync(context, userManager, options);
+        await CreateDoctorsAsync(context, userManager, options);
+        await CreateDoctorsAsync(context, userManager, options);
     }
 
-    private static void CreateOilMarks(ICheckDriveDbContext context)
+    private static async Task CreateOilMarksAsync(ICheckDriveDbContext context)
     {
-        if (context.OilMarks.Any()) return;
+        if (context.OilMarks.Any())
+        {
+            return;
+        }
 
         foreach (var oilMarkName in _oilMarks)
         {
@@ -37,12 +48,15 @@ internal sealed class DevelopmentDatabaseSeeder : IDatabaseSeeder
             context.OilMarks.Add(oilMark);
         }
 
-        context.SaveChanges();
+        await context.SaveChangesAsync();
     }
 
-    private static void CreateCars(ICheckDriveDbContext context, DataSeedOptions options)
+    private static async Task CreateCarsAsync(ICheckDriveDbContext context, DataSeedOptions options)
     {
-        if (context.Cars.Any()) return;
+        if (context.Cars.Any())
+        {
+            return;
+        }
 
         var uniqueCarsByName = new Dictionary<string, Car>();
         var oilMarks = context.OilMarks.Select(x => x.Id).ToList();
@@ -57,14 +71,16 @@ internal sealed class DevelopmentDatabaseSeeder : IDatabaseSeeder
             }
         }
 
-        context.SaveChanges();
+        await context.SaveChangesAsync();
     }
 
-    private static void CreateDrivers(ICheckDriveDbContext context, UserManager<IdentityUser> userManager, DataSeedOptions options)
+    private static async Task CreateDriversAsync(ICheckDriveDbContext context, UserManager<IdentityUser> userManager, DataSeedOptions options)
     {
-        if (context.Drivers.Any()) return;
+        if (context.Drivers.Any())
+        {
+            return;
+        }
 
-        var role = context.Roles.First(x => x.Name == "driver");
         var uniqueDriversByName = new Dictionary<string, Driver>();
         var carIds = context.Cars.Select(x => x.Id).ToList();
 
@@ -88,23 +104,19 @@ internal sealed class DevelopmentDatabaseSeeder : IDatabaseSeeder
             }
         }
 
-        context.SaveChanges();
+        await context.SaveChangesAsync();
+
         var drivers = context.Drivers.ToArray();
-
-        foreach (var driver in drivers)
-        {
-            var userRole = new IdentityUserRole<string> { RoleId = role.Id, UserId = driver.AccountId };
-            context.UserRoles.Add(userRole);
-        }
-
-        context.SaveChanges();
+        await AssignToRole(context, drivers, "driver");
     }
 
-    private static void CreateDoctors(ICheckDriveDbContext context, UserManager<IdentityUser> userManager, DataSeedOptions options)
+    private static async Task CreateDoctorsAsync(ICheckDriveDbContext context, UserManager<IdentityUser> userManager, DataSeedOptions options)
     {
-        if (context.Doctors.Any()) return;
+        if (context.Doctors.Any())
+        {
+            return;
+        }
 
-        var role = context.Roles.First(x => x.Name == "doctor");
         var uniqueDoctorsByName = new Dictionary<string, Doctor>();
 
         for (int i = 0; i < options.DoctorsCount; i++)
@@ -126,23 +138,19 @@ internal sealed class DevelopmentDatabaseSeeder : IDatabaseSeeder
             }
         }
 
-        context.SaveChanges();
+        await context.SaveChangesAsync();
+
         var doctors = context.Doctors.ToArray();
-
-        foreach (var doctor in doctors)
-        {
-            var userRole = new IdentityUserRole<string> { RoleId = role.Id, UserId = doctor.AccountId };
-            context.UserRoles.Add(userRole);
-        }
-
-        context.SaveChanges();
+        await AssignToRole(context, doctors, "doctor");
     }
 
-    private static void CreateMechanics(ICheckDriveDbContext context, UserManager<IdentityUser> userManager, DataSeedOptions options)
+    private static async Task CreateMechanicsAsync(ICheckDriveDbContext context, UserManager<IdentityUser> userManager, DataSeedOptions options)
     {
-        if (context.Mechanics.Any()) return;
+        if (context.Mechanics.Any())
+        {
+            return;
+        }
 
-        var role = context.Roles.First(x => x.Name == "mechanic");
         var uniqueMechanicsByName = new Dictionary<string, Mechanic>();
 
         for (int i = 0; i < options.MechanicsCount; i++)
@@ -164,23 +172,19 @@ internal sealed class DevelopmentDatabaseSeeder : IDatabaseSeeder
             }
         }
 
-        context.SaveChanges();
+        await context.SaveChangesAsync();
+
         var mechanics = context.Mechanics.ToArray();
-
-        foreach (var mechanic in mechanics)
-        {
-            var userRole = new IdentityUserRole<string> { RoleId = role.Id, UserId = mechanic.AccountId };
-            context.UserRoles.Add(userRole);
-        }
-
-        context.SaveChanges();
+        await AssignToRole(context, mechanics, "mechanic");
     }
 
-    private static void CreateOperators(ICheckDriveDbContext context, UserManager<IdentityUser> userManager, DataSeedOptions options)
+    private static async Task CreateOperatorsAsync(ICheckDriveDbContext context, UserManager<IdentityUser> userManager, DataSeedOptions options)
     {
-        if (context.Operators.Any()) return;
+        if (context.Operators.Any())
+        {
+            return;
+        }
 
-        var role = context.Roles.First(x => x.Name == "operator");
         var uniqueOperatorsByName = new Dictionary<string, Operator>();
 
         for (int i = 0; i < options.OperatorsCount; i++)
@@ -202,23 +206,19 @@ internal sealed class DevelopmentDatabaseSeeder : IDatabaseSeeder
             }
         }
 
-        context.SaveChanges();
+        await context.SaveChangesAsync();
+
         var operators = context.Operators.ToArray();
-
-        foreach (var @operator in operators)
-        {
-            var userRole = new IdentityUserRole<string> { RoleId = role.Id, UserId = @operator.AccountId };
-            context.UserRoles.Add(userRole);
-        }
-
-        context.SaveChanges();
+        await AssignToRole(context, operators, "operator");
     }
 
-    private static void CreateDispatchers(ICheckDriveDbContext context, UserManager<IdentityUser> userManager, DataSeedOptions options)
+    private static async Task CreateDispatchersAsync(ICheckDriveDbContext context, UserManager<IdentityUser> userManager, DataSeedOptions options)
     {
-        if (context.Dispatchers.Any()) return;
+        if (context.Dispatchers.Any())
+        {
+            return;
+        }
 
-        var role = context.Roles.First(x => x.Name == "dispatcher");
         var uniqueDispatchersByName = new Dictionary<string, Dispatcher>();
 
         for (int i = 0; i < options.DispatchersCount; i++)
@@ -240,23 +240,19 @@ internal sealed class DevelopmentDatabaseSeeder : IDatabaseSeeder
             }
         }
 
-        context.SaveChanges();
+        await context.SaveChangesAsync();
+
         var dispatchers = context.Dispatchers.ToArray();
-
-        foreach (var dispatcher in dispatchers)
-        {
-            var userRole = new IdentityUserRole<string> { RoleId = role.Id, UserId = dispatcher.AccountId };
-            context.UserRoles.Add(userRole);
-        }
-
-        context.SaveChanges();
+        await AssignToRole(context, dispatchers, "dispatcher");
     }
 
-    private static void CreateManagers(ICheckDriveDbContext context, UserManager<IdentityUser> userManager, DataSeedOptions options)
+    private static async Task CreateManagers(ICheckDriveDbContext context, UserManager<IdentityUser> userManager, DataSeedOptions options)
     {
-        if (context.Managers.Any()) return;
+        if (context.Managers.Any())
+        {
+            return;
+        }
 
-        var role = context.Roles.First(x => x.Name == "manager");
         var uniqueManagersByName = new Dictionary<string, Manager>();
 
         for (int i = 0; i < options.ManagersCount; i++)
@@ -278,15 +274,99 @@ internal sealed class DevelopmentDatabaseSeeder : IDatabaseSeeder
             }
         }
 
-        context.SaveChanges();
-        var managers = context.Managers.ToArray();
+        await context.SaveChangesAsync();
 
-        foreach (var manager in managers)
+        var managers = context.Managers.ToArray();
+        await AssignToRole(context, managers, "manager");
+    }
+
+    private static void CreateCheckPoints(ICheckDriveDbContext context)
+    {
+        if (context.CheckPoints.Any())
         {
-            var userRole = new IdentityUserRole<string> { RoleId = role.Id, UserId = manager.AccountId };
+            return;
+        }
+
+        var startDate = DateTime.Now.AddYears(-2);
+        var drivers = context.Drivers.ToArray();
+
+        while (startDate != DateTime.Now)
+        {
+            if (startDate.DayOfWeek == DayOfWeek.Sunday)
+            {
+                continue;
+            }
+
+            foreach (var driver in drivers)
+            {
+                CreateCheckPoint(context, driver, startDate);
+            }
+
+            var daysToAdd = new Random().Next(1, 3);
+            startDate = startDate.AddDays(daysToAdd);
+        }
+    }
+
+    private static async Task AssignToRole(
+        ICheckDriveDbContext context,
+        Employee[] employees,
+        string roleTitle)
+    {
+        var role = context.Roles.First(x => x.Name == roleTitle);
+
+        if (role is null)
+        {
+            return;
+        }
+
+        foreach (var employee in employees)
+        {
+            var userRole = new IdentityUserRole<string> { RoleId = role.Id, UserId = employee.AccountId };
             context.UserRoles.Add(userRole);
         }
 
-        context.SaveChanges();
+        await context.SaveChangesAsync();
+    }
+
+    private static void CreateCheckPoint(ICheckDriveDbContext context, Driver driver, DateTime dateTime)
+    {
+        var startDate = new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, 8, 15, 0);
+        var operators = context.Operators.ToArray();
+        var mechanics = context.Mechanics.ToArray();
+        var dispatchers = context.Dispatchers.ToArray();
+        var managers = context.Managers.ToArray();
+
+        var doctorReview = CreateDoctorReview(context, driver, startDate);
+
+        if (!doctorReview.IsHealthy)
+        {
+            var checkPoint = new CheckPoint
+            {
+                DoctorReview = doctorReview,
+                StartDate = startDate,
+                Stage = Domain.Enums.CheckPointStage.DoctorReview,
+                Status = Domain.Enums.CheckPointStatus.Completed,
+            };
+            context.CheckPoints.Add(checkPoint);
+            context.SaveChanges();
+
+            return;
+        }
+    }
+
+    private static DoctorReview CreateDoctorReview(ICheckDriveDbContext context, Driver driver, DateTime dateTime)
+    {
+        var doctors = context.Doctors.ToArray();
+
+        var review = new DoctorReview
+        {
+            Doctor = _faker.PickRandom(doctors),
+            Driver = driver,
+            CheckPoint = null!,
+            Date = _faker.Date.Between(dateTime, dateTime),
+            IsHealthy = _faker.Random.Bool(),
+        };
+
+        return review;
     }
 }
